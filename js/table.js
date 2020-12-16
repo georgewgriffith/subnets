e5c5d7d6b396b4b82af5c5f0464e24665d503572
George Griffith <georgewgriffith@gmail.com>
2020-12-16 21:57:25 +0000

    Experimentation

--- js/table.js
+++ js/table.js
@@ -2,7 +2,47 @@ $("input:checkbox:not(:checked)").each(function () {
   var column = "table ." + $(this).attr("name");
   $(column).hide();
 });
+
 $("input:checkbox").click(function () {
   var column = "table ." + $(this).attr("name");
   $(column).toggle();
 });
+
+$(function() {
+  // on init
+  $(".table-hideable .hide-col").each(HideColumnIndex);
+
+  // on click
+  $('.hide-column').click(HideColumnIndex)
+
+  function HideColumnIndex() {
+    var $el = $(this);
+    var $cell = $el.closest('th,td')
+    var $table = $cell.closest('table')
+
+    // get cell location - https://stackoverflow.com/a/4999018/1366033
+    var colIndex = $cell[0].cellIndex + 1;
+
+    // find and hide col index
+    $table.find("tbody tr, thead tr")
+      .children(":nth-child(" + colIndex + ")")
+      .addClass('hide-col');
+      
+    // show restore footer
+    $table.find(".footer-restore-columns").show()
+  }
+
+  // restore columns footer
+  $(".restore-columns").click(function(e) {
+    var $table = $(this).closest('table')
+    $table.find(".footer-restore-columns").hide()
+    $table.find("th, td")
+      .removeClass('hide-col');
+
+  })
+
+  $('[data-toggle="tooltip"]').tooltip({
+    trigger: 'hover'
+  })
+
+})
